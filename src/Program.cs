c05f5e4ab7d0fafb6541072c595f62d7d5eb7ea0
Jordan Bean <jordanbean@microsoft.com>
2023-06-14 16:36:15 -0500

    v3

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,32 @@
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+                  services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+                          {
+                            var options = new OpenApiConfigurationOptions()
+                            {
+                              Info = new OpenApiInfo()
+                              {
+                                Version = DefaultOpenApiConfigurationOptions.GetOpenApiDocVersion(),
+                                Title = $"{DefaultOpenApiConfigurationOptions.GetOpenApiDocTitle()} (Injected)",
+                                Description = DefaultOpenApiConfigurationOptions.GetOpenApiDocDescription(),
+                              },
+                              Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                              OpenApiVersion = OpenApiVersionType.V3,
+                              IncludeRequestingHostName = DefaultOpenApiConfigurationOptions.IsFunctionsRuntimeEnvironmentDevelopment()
+                            };
+                            return options;
+                          });
+                })
     .Build();
 
 host.Run();
